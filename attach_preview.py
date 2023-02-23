369a6bc89c4c238a8d6811500453d140b73e70bc
Ryan Maffesoli <rmaffesoli@perforce.com>
2023-02-23 11:56:08 -0500

    var names

--- attach_preview.py
+++ attach_preview.py
@@ -2,9 +2,6 @@
 from __future__ import print_function
 
 import os
-import random
-import tempfile
-import string
 import binascii
 
 import subprocess
@@ -13,8 +10,8 @@ import argparse
 
 def attach_preview_attr(selected_asset, img_path):
     file_dir, file_name = os.path.split(selected_asset)
-    with open(img_path, "rb") as f:
-        hex_string = binascii.hexlify(f.read())
+    with open(img_path, "rb") as file_buff:
+        hex_string = binascii.hexlify(file_buff.read())
 
         for attr in ["preview", "thumb", "blur"]:
             commands = ["p4", "attribute", "-fei", "-n", attr, file_name]
@@ -22,7 +19,7 @@ def attach_preview_attr(selected_asset, img_path):
             if attr == "blur":
                 commands = ["p4", "attribute", "-fi", "-n", attr, file_name]
                 hex_string = bytes("U4DbZs009u=X7O9a599t=EtQ~U-U01~C0Mxa", "utf-8")
-            p = subprocess.Popen(
+            proc = subprocess.Popen(
                 commands,
                 stdout=subprocess.PIPE,
                 stdin=subprocess.PIPE,
@@ -30,7 +27,7 @@ def attach_preview_attr(selected_asset, img_path):
                 cwd=file_dir,
             )
 
-            attr_stdout = p.communicate(input=hex_string)[0]
+            attr_stdout = proc.communicate(input=hex_string)[0]
             print(attr_stdout.decode())
 
 
