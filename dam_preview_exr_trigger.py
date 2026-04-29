f1177e79d4f46189f069658a1067acb7b1e22438
Ryan Maffesoli <pairorat@gmail.com>
2026-04-29 13:19:06 -0400

    exr trigger

--- dam_preview_exr_trigger.py
+++ dam_preview_exr_trigger.py
@@ -1,37 +1,44 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-####################################################################################################
-# (c) Marwan Abdellah, Blue Brain Project, Ecole Polytechnique Federal de Lausanne (EPFL) 
-####################################################################################################
-
-# Imports 
 import binascii
 import os
 import subprocess
 import random
+import logging
 import string
 import tempfile
+import argparse
+from wand.image import Image
 
 from P4 import P4, P4Exception
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+fh = logging.FileHandler('/home/perforce/triggers/create_exr_thumbnails/create_exr_thumbnails.log')
+logger.addHandler(fh)
+
 p4 = P4()
 p4.user = 'username'
-p4.port = 'p4_port'
+p4.port = 'p4port'
 p4.connect()
 
 def gen_exr_dam_preview_attr(depot_path):
     with tempfile.TemporaryDirectory() as temp_dir:
         filepath = os.path.join(temp_dir, get_random_word())
         img_path = ".".join([filepath, "png"])
-    
+
         temp_exr = os.path.join(temp_dir,"temp.exr")
         p4.run("print", "-q", "-o", f"{temp_exr}", depot_path)
 
-        
-        conversion_args = '-gamma %s -channel ALL -normalize -quality 100' % (1.5)
-        shell_command = 'magick convert %s %s %s ' % (temp_exr, conversion_args, img_path)
-        print('CONVERTING: ' + shell_command)
-        subprocess.call(shell_command, shell=True)
+        with Image(filename=temp_exr) as img:
+            img.colorspace = 'rgb'
+            img.transform_colorspace('srgb')
+            img.format = 'png'
+            img.save(filename=img_path)
+
+        logger.info('CONVERSION DONE')
+        logger.info('img_path: %s, exists: %s', img_path, os.path.exists(img_path))
+        logger.info('temp_exr: %s, exists: %s', temp_exr, os.path.exists(temp_exr))
 
         with open(img_path, "rb") as file_buff:
             hex_string = binascii.hexlify(file_buff.read())
@@ -44,16 +51,13 @@ def gen_exr_dam_preview_attr(depot_path):
                 hex_string,
                 f"{depot_path}",
                 )
-            print("Attributes set.")
-    
+            logger.info("Attributes set.")
+
 
 def get_random_word(length=12):
     return "".join(random.sample(string.ascii_letters, length))
 
-     
-####################################################################################################
-# @main
-####################################################################################################
+
 def main(changelist):
     description = p4.run_describe(changelist)
     for i, depot_file in enumerate(description[0]["depotFile"]):
@@ -63,4 +67,8 @@ def main(changelist):
 
 
 if __name__ == '__main__':
-    main(2790)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("changelist")
+
+    parsed_args = parser.parse_args()
+    main(parsed_args.changelist)
